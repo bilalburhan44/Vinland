a47e57ed760cd9de230aba84c2a02d19603fd075
bilalburhan44 <110193290+bilalburhan44@users.noreply.github.com>
2024-07-07 20:52:51 +0300

    Add files via upload

---
+++ Client/src/layouts/authentication/sign-in/index.js
@@ -0,0 +1,121 @@
+/**
+=========================================================
+* Soft UI Dashboard React - v4.0.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useState, useEffect } from "react";
+
+// react-router-dom components
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { LoginUser } from "../../../apicalls/users";
+// @mui material components
+import Switch from "@mui/material/Switch";
+
+// Soft UI Dashboard React components
+import SoftBox from "components/SoftBox";
+import SoftTypography from "components/SoftTypography";
+import SoftInput from "components/SoftInput";
+import SoftButton from "components/SoftButton";
+
+// Authentication layout components
+import CoverLayout from "layouts/authentication/components/CoverLayout";
+
+// Images
+import curved9 from "assets/images/curved-images/curved-6.jpg";
+
+function SignIn() {
+  const [rememberMe, setRememberMe] = useState(true);
+  const location = useLocation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSetRememberMe = () => setRememberMe(!rememberMe);
+
+
+const onsubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await LoginUser({ email, password });
+      if (response.success) {
+        localStorage.setItem("token", response.data);
+        navigate("/dashboard");
+      } else {
+        throw new Error(response.message);
+      }
+    } catch (error) {
+      console.error("Error logging in:", error);
+    }
+  };
+
+
+  return (
+    <CoverLayout
+      title="Welcome back"
+      description="Enter your email and password to sign in"
+      image={curved9}
+    >
+      <SoftBox component="form" role="form" onSubmit={onsubmit}>
+        <SoftBox mb={2}>
+          <SoftBox mb={1} ml={0.5}>
+            <SoftTypography component="label" variant="caption" fontWeight="bold">
+              Email
+            </SoftTypography>
+          </SoftBox>
+          <SoftInput type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </SoftBox>
+        <SoftBox mb={2}>
+          <SoftBox mb={1} ml={0.5}>
+            <SoftTypography component="label" variant="caption" fontWeight="bold">
+              Password
+            </SoftTypography>
+          </SoftBox>
+          <SoftInput type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </SoftBox>
+        <SoftBox display="flex" alignItems="center">
+          <Switch checked={rememberMe} onChange={handleSetRememberMe} />
+          <SoftTypography
+            variant="button"
+            fontWeight="regular"
+            onClick={handleSetRememberMe}
+            sx={{ cursor: "pointer", userSelect: "none" }}
+          >
+            &nbsp;&nbsp;Remember me
+          </SoftTypography>
+        </SoftBox>
+        <SoftBox mt={4} mb={1}>
+          <SoftButton variant="gradient" color="info" fullWidth type="submit">
+            sign in
+          </SoftButton>
+        </SoftBox>
+        <SoftBox mt={3} textAlign="center">
+          <SoftTypography variant="button" color="text" fontWeight="regular">
+            Don&apos;t have an account?{" "}
+            <SoftTypography
+              component={Link}
+              to="/authentication/sign-up"
+              variant="button"
+              color="info"
+              fontWeight="medium"
+              textGradient
+            >
+              Sign up
+            </SoftTypography>
+          </SoftTypography>
+        </SoftBox>
+      </SoftBox>
+    </CoverLayout>
+  );
+}
+
+export default SignIn;
