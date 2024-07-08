84ca0844abea7f95a5974df56c48ada3e137c8fb
bilalburhan44 <110193290+bilalburhan44@users.noreply.github.com>
2024-07-08 23:03:34 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,15 @@ app.use('/api/users', userRoutes);
 app.use('/api/clients', clientRoutes);
 app.use('/api/wallet', walletRoutes);
 
-// ... other middleware and routes
+//deploy config
+const path = require('path');
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,'client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
