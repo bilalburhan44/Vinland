c3fb70c386923c4d4d5d84c6f8676814efad7cc4
bilalburhan44 <110193290+bilalburhan44@users.noreply.github.com>
2024-07-24 19:06:24 +0300

    Update index.js

--- Client/src/layouts/dashboard/index.js
+++ Client/src/layouts/dashboard/index.js
@@ -26,21 +26,18 @@ import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import MiniStatisticsCard from "examples/Cards/StatisticsCards/MiniStatisticsCard";
-import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
-import GradientLineChart from "examples/Charts/LineCharts/GradientLineChart";
+
 
 // Soft UI Dashboard React base styles
 import typography from "assets/theme/base/typography";
 
 // Dashboard layout components
-import BuildByDevelopers from "layouts/dashboard/components/BuildByDevelopers";
-import WorkWithTheRockets from "layouts/dashboard/components/WorkWithTheRockets";
+
 import Projects from "layouts/dashboard/components/Projects";
 import OrderOverview from "layouts/dashboard/components/OrderOverview";
 
 // Data
 import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
-import gradientLineChartData from "layouts/dashboard/data/gradientLineChartData";
 import { getWallet } from "../../apicalls/wallet";
 
 import React, { useEffect, useState } from "react";
@@ -49,8 +46,7 @@ import SoftButton from "components/SoftButton";
 import ChangeCurrencyModal from "components/Modal/changeCurrencyModal";
 
 function Dashboard() {
-  const { size } = typography;
-  const { chart, items } = reportsBarChartData;
+
   const [wallet, setWallet] = useState({});
   const [exchangeRate, setExchangeRate] = useState(null);
   const [open, setOpen] = useState(false);
@@ -162,42 +158,7 @@ function Dashboard() {
             </Grid>
           </Grid>
               </SoftBox>*/}
-        <SoftBox mb={3}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} lg={5}>
-              <ReportsBarChart
-                title="active users"
-                description={
-                  <>
-                    (<strong>+23%</strong>) than last week
-                  </>
-                }
-                chart={chart}
-                items={items}
-              />
-            </Grid>
-            <Grid item xs={12} lg={7}>
-              <GradientLineChart
-                title="Sales Overview"
-                description={
-                  <SoftBox display="flex" alignItems="center">
-                    <SoftBox fontSize={size.lg} color="success" mb={0.3} mr={0.5} lineHeight={0}>
-                      <Icon className="font-bold">arrow_upward</Icon>
-                    </SoftBox>
-                    <SoftTypography variant="button" color="text" fontWeight="medium">
-                      4% more{" "}
-                      <SoftTypography variant="button" color="text" fontWeight="regular">
-                        in 2021
-                      </SoftTypography>
-                    </SoftTypography>
-                  </SoftBox>
-                }
-                height="20.25rem"
-                chart={gradientLineChartData}
-              />
-            </Grid>
-          </Grid>
-        </SoftBox>
+        
         <Grid container spacing={3}>
           <Grid item xs={12} md={6} lg={8}>
             <Projects />
