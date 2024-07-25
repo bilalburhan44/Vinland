f139234e6c5f8800316ae44a364c7950d5c026f7
bilalburhan44 <110193290+bilalburhan44@users.noreply.github.com>
2024-07-25 18:45:17 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,9 +25,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-// Manually handle preflight requests
-app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions)); // Handle preflight requests
 
 app.use(express.json());
 
