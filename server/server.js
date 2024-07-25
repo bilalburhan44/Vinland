2885ef021fd2bcd58053c06082e8724d39699f80
bilalburhan44 <110193290+bilalburhan44@users.noreply.github.com>
2024-07-25 10:40:52 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,17 +24,21 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-// Manually handle preflight requests
 app.options('*', cors(corsOptions));
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Request received:', req.method, req.path);
+  console.log(`Request received: ${req.method} ${req.path}`);
   next();
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://main--vinlandkitchen.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 // Define your routes
 app.use('/api/exchangeRate', exchangeRateRoutes);
