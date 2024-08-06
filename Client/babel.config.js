2325273fdfc9bb157347418c5d3230139b4f02a6
bilalburhan44 <110193290+bilalburhan44@users.noreply.github.com>
2024-08-06 17:42:50 +0300

    Update babel.config.js

--- Client/babel.config.js
+++ Client/babel.config.js
@@ -11,10 +11,6 @@ module.exports = function (api) {
     '@babel/plugin-transform-private-methods',
   ];
 
-  if (process.env.NODE_ENV === 'development') {
-    plugins.push(require('react-refresh/babel'));
-  }
-
   return {
     presets,
     plugins,
