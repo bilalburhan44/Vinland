e286d1241e31949646c97de91291fefcc6105021
bilalburhan44 <110193290+bilalburhan44@users.noreply.github.com>
2024-08-06 15:05:09 +0300

    Add files via upload

--- Client/babel.config.js
+++ Client/babel.config.js
@@ -1,13 +1,22 @@
-module.exports = {
-    presets: ['react-app'],
-    plugins: [
-      '@babel/plugin-proposal-private-property-in-object',
-      '@babel/plugin-transform-class-properties',
-      '@babel/plugin-transform-nullish-coalescing-operator',
-      '@babel/plugin-transform-numeric-separator',
-      '@babel/plugin-transform-optional-chaining',
-      '@babel/plugin-transform-private-methods',
-      process.env.NODE_ENV === 'development' && require('react-refresh/babel'),
-    ].filter(Boolean)
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = ['react-app'];
+  const plugins = [
+    '@babel/plugin-proposal-private-property-in-object',
+    '@babel/plugin-transform-class-properties',
+    '@babel/plugin-transform-nullish-coalescing-operator',
+    '@babel/plugin-transform-numeric-separator',
+    '@babel/plugin-transform-optional-chaining',
+    '@babel/plugin-transform-private-methods',
+  ];
+
+  if (process.env.NODE_ENV === 'development') {
+    plugins.push(require('react-refresh/babel'));
+  }
+
+  return {
+    presets,
+    plugins,
   };
-  
+};
