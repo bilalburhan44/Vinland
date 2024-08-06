1c440271fd5e09ede0bd01caf23ba8371ff87e82
bilalburhan44 <110193290+bilalburhan44@users.noreply.github.com>
2024-08-06 16:12:08 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const presets = ['react-app',"@babel/preset-env", "@babel/preset-react"];
+  const presets = ['react-app', "@babel/preset-env", "@babel/preset-react"];
   const plugins = [
-    '@babel/plugin-proposal-private-property-in-object',
     '@babel/plugin-transform-class-properties',
     '@babel/plugin-transform-nullish-coalescing-operator',
     '@babel/plugin-transform-numeric-separator',
     '@babel/plugin-transform-optional-chaining',
     '@babel/plugin-transform-private-methods',
-    process.env.NODE_ENV === 'development' ? ['react-refresh/babel'] : []
-  ];
+    process.env.NODE_ENV === 'development' ? 'react-refresh/babel' : null,
+  ].filter(Boolean);
 
   return {
     presets,
