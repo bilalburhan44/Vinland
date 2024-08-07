6735089c637cc09ef9be4c068d8e0109d648aa6c
bilalburhan44 <110193290+bilalburhan44@users.noreply.github.com>
2024-08-07 12:26:04 +0300

    Update axiosinstance.js

--- Client/src/apicalls/axiosinstance.js
+++ Client/src/apicalls/axiosinstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // or your actual backend URL
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000', // Use env variable or fallback to localhost
   headers: {
     'Content-Type': 'application/json',
 },
