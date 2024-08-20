6b0a545efe989fbd9d6106337e713682cbd66955
bilalburhan44 <110193290+bilalburhan44@users.noreply.github.com>
2024-08-20 22:37:10 +0300

    Update users.js

--- Client/src/apicalls/users.js
+++ Client/src/apicalls/users.js
@@ -41,4 +41,34 @@ export const RegisterUser = async(payload) => {
       return error.message;
     }
   };
+
+  // get all users
+  export const getAllUsers = async() => {
+    try {
+      const response = await axiosInstance.get('/api/users/getAllUsers');
+      return response.data;
+    } catch (error) {
+      return error.message;
+    }
+  };
+
+  // delete user
+  export const deleteUser = async(id) => {
+    try {
+      const response = await axiosInstance.delete(`/api/users/deleteUser/${id}`);
+      return response.data;
+    } catch (error) {
+      return error.message;
+    }
+  };
+
+  // update user
+  export const updateUser = async(id, payload) => {
+    try {
+      const response = await axiosInstance.put(`/api/users/updateUser/${id}`, payload);
+      return response.data;
+    } catch (error) {
+      return error.message;
+    }
+  };
   
